c91745573e2c50bf565df7af7498e4ed07753e03
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-09-10 15:26:48 +0545

    ft: minor changes

--- flood.py
+++ flood.py
@@ -6,50 +6,95 @@ from obspy.clients.fdsn import Client
 
 client = Client("EARTHSCOPE")
 
-t_start = UTCDateTime("2026-08-26T01:30:00")
-t_end   = UTCDateTime("2026-08-26T04:00:00")
+start = UTCDateTime("2026-08-26T02:00:00")
+end   = UTCDateTime("2026-08-26T04:10:00")
 
-st = client.get_waveforms(network="IO", station="EVN", location="*", channel="HHZ, BHZ", starttime=t_start, endtime=t_end)
+st = client.get_waveforms(network="IO", station="EVN", location="*", channel="HHZ", starttime=start, endtime=end)
 
-# Fill gaps and select high-rate vertical channel (HHZ)
 st.merge(fill_value="latest")
-tr = st.select(channel="HHZ")[0]
+tr = st[0].copy()
 
-tr1 = tr.copy()
-tr1.detrend("demean")
-tr1.detrend("linear")
-tr1.filter("bandpass", freqmin=0.5, freqmax=10.0, corners=4, zerophase=True)
+tr.detrend("demean")
+tr.detrend("linear")
+tr.filter("bandpass", freqmin=0.5, freqmax=10.0, corners=4, zerophase=True)
 
 # Print Trace Metadata
-fs = tr1.stats.sampling_rate
-print(f"Channel: {tr1.id}")
-print(f"Sampling Rate: {fs} Hz")
-print(f"Number of Samples: {tr1.stats.npts}")
-print(f"Max Amplitude: {tr1.data.max():.2f} counts")
+print(f"Channel: {tr.id}")
+print(f"Sampling Rate: {tr.stats.sampling_rate} Hz")
+print(f"Number of Samples: {tr.stats.npts}")
 
-# Compute Spectrogram via SciPy
-# Using 2-second window (2 * fs) with 85% overlap for sharp HHZ temporal resolution
-f, t, Sxx = spectrogram(
-    tr1.data,
-    fs=fs,
-    nperseg=int(2 * fs),
-    noverlap=int(1.7 * fs)
+sampling_rate = tr.stats.sampling_rate
+
+time = np.arange(tr.stats.npts) / sampling_rate
+
+fig, (ax1, ax2) = plt.subplots(2,1,figsize=(14, 9),gridspec_kw={"height_ratios": [1, 2]})
+
+# waveformm
+
+ax1.plot(time, tr.data)
+
+ax1.set_xlabel("Time (seconds)")
+ax1.set_ylabel("Amplitude")
+
+ax1.set_title("Waveform")
+
+ax1.grid(True)
+
+# plt.tight_layout()
+# plt.show()
+
+# spectogram
+
+frequencies, times, Sxx = spectrogram(
+    tr.data,
+    fs=sampling_rate,
+    nperseg=1024,
+    noverlap=512
 )
 
-# Combined Plot: Time Series Waveform & Spectrogram
-fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 7), gridspec_kw={'height_ratios': [1, 2]}
-)
 
-# Top Subplot: Bandpass-Filtered Time-Series
-ax1.plot(tr1.times(), tr1.data, color="black", lw=0.6)
-ax1.set_ylabel("Counts")
-ax1.set_title(f"{tr1.id} — Waveform & Spectrogram Analysis ({tr1.stats.starttime.strftime('%Y-%m-%d %H:%M:%S UTC')})")
-ax1.grid(True, linestyle="--", alpha=0.5)
+# pcm = ax2.pcolormesh(
+#     times,
+#     frequencies,
+#     10 * np.log10(Sxx + 1e-10),
+#     shading="gouraud", 
+#     cmap="magma"
+# )
 
-# Bottom Subplot: Spectrogram in Decibels (dB)
+# ax2.set_xlabel("Time (seconds)")
+# ax2.set_ylabel("Frequency (Hz)")
+
+# ax2.set_title("Spectrogram")
+
+# # fig.colorbar(label="Power (dB)")
+
+# ax2.set_ylim(0, 10)
+
+# fig.colorbar(
+#     pcm,
+#     ax=ax2,
+#     label="Power (dB)"
+# )
+
+
+# plt.tight_layout()
+# plt.show()
+
+
+# # Combined Plot: Time Series Waveform & Spectrogram
+# fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 7), gridspec_kw={'height_ratios': [1, 2]}
+# )
+
+# # Top Subplot: Bandpass-Filtered Time-Series
+# ax1.plot(tr1.times(), tr1.data, color="black", lw=0.6)
+# ax1.set_ylabel("Counts")
+# ax1.set_title(f"{tr1.id} — Waveform & Spectrogram Analysis ({tr1.stats.starttime.strftime('%Y-%m-%d %H:%M:%S UTC')})")
+# ax1.grid(True, linestyle="--", alpha=0.5)
+
+# # Bottom Subplot: Spectrogram in Decibels (dB)
 
 pcm = ax2.pcolormesh(
-    t, f, 
+    times, frequencies, 
     10 * np.log10(Sxx + 1e-10),  # Convert power spectrum to dB
     shading="gouraud", 
     cmap="magma"
--- stations.py
+++ stations.py
@@ -1,21 +1,34 @@
-import matplotlib.pyplot as plt
 from obspy import UTCDateTime
 from obspy.clients.fdsn import Client
 
 client = Client("EARTHSCOPE")
 
-t_start = UTCDateTime("2026-08-26T01:30:00")
-t_end = UTCDateTime("2026-08-26T04:00:00")
+start = UTCDateTime("2026-08-26T00:00:00")
+end   = UTCDateTime("2026-08-26T04:00:00")
 
-st = client.get_waveforms(
-    network="IO",
-    station="EVN",
-    location="*",
-    channel="HHZ, BHZ",
-    starttime=t_start,
-    endtime=t_end,
-)
+# inventory = client.get_stations(latitude=28.2500, longitude=83.9400, maxradius=5, starttime=start, endtime=end, level="channel")
 
-st.merge(fill_value="latest")
+# print(inventory) 
 
-print(st)
+
+stations = [
+    ("IO", "EVN", "", "HHZ"),
+    ("NK", "KKN", "", "BHZ"),
+    ("NQ", "KNSET", "01", "HNZ"),
+    ("NQ", "KTNP2", "01", "HNZ"),
+]
+
+
+for network, station, location, channel in stations:
+
+    print(f"{network}.{station}.{location}.{channel}")
+    st = client.get_waveforms(
+            network=network,
+            station=station,
+            location=location,
+            channel=channel,
+            starttime=start,
+            endtime=end
+        )
+
+    print(st)
