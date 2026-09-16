b6e55f3916d244284b31361b1d8a2a4332a2974e
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-09-16 17:35:11 +0545

    minor changes

--- stalta.py
+++ stalta.py
@@ -5,7 +5,8 @@ from obspy import UTCDateTime
 from obspy.clients.fdsn import Client
 from obspy.signal.cross_correlation import correlate, xcorr_max
 import matplotlib.dates as mdates
-from obspy.signal.trigger import classic_sta_lta, trigger_onset
+from obspy.signal.trigger import classic_sta_lta, trigger_onset, recursive_sta_lta
+import matplotlib.pyplot as plt
 
 
 client = Client("https://seiscomp.alertnepal.online")
@@ -17,94 +18,72 @@ end   = UTCDateTime("2026-08-26T02:53:00")
 
 st_kkn = client.get_waveforms(network="NK", station="KKN", location="*", channel="BHZ", starttime=start, endtime=end)
 
-st_evn = client.get_waveforms(network="IO", station="EVN", location="*", channel="HHZ", starttime=start, endtime=end)
+st_evn = client.get_waveforms(network="IO", station="EVN", location="*", channel="BHZ", starttime=start, endtime=end)
 
 tr_kkn = st_kkn[0]
 tr_evn = st_evn[0]
 
-tr_kkn.filter("bandpass", freqmin=1.0, freqmax=10.0)
-tr_evn.filter("bandpass", freqmin=1.0, freqmax=10.0)
+tr_kkn.detrend("demean").detrend("linear")
+tr_evn.detrend("demean").detrend("linear")
+
+tr_kkn.filter("bandpass", freqmin=2.0, freqmax=8.0)
+tr_evn.filter("bandpass", freqmin=2.0, freqmax=8.0)
 
 fs_kkn = tr_kkn.stats.sampling_rate
 fs_evn = tr_evn.stats.sampling_rate
 
-sta_seconds = 1
-lta_seconds = 10
+cft_kkn = recursive_sta_lta(tr_kkn.data, int(0.5 * fs_kkn), int(5.0 * fs_kkn))
+cft_evn = recursive_sta_lta(tr_evn.data, int(0.5 * fs_evn), int(5.0 * fs_evn))
+# sta_seconds = 1
+# lta_seconds = 10
 
-# KKN
-sta_kkn = int(sta_seconds * fs_kkn)
-lta_kkn = int(lta_seconds * fs_kkn)
+# # KKN
+# sta_kkn = int(sta_seconds * fs_kkn)
+# lta_kkn = int(lta_seconds * fs_kkn)
 
-cft_kkn = classic_sta_lta(
-    tr_kkn.data,
-    sta_kkn,
-    lta_kkn
-)
+# cft_kkn = classic_sta_lta(
+#     tr_kkn.data,
+#     sta_kkn,
+#     lta_kkn
+# )
 
-# EVN
-sta_evn = int(sta_seconds * fs_evn)
-lta_evn = int(lta_seconds * fs_evn)
+# # EVN
+# sta_evn = int(sta_seconds * fs_evn)
+# lta_evn = int(lta_seconds * fs_evn)
 
-cft_evn = classic_sta_lta(
-    tr_evn.data,
-    sta_evn,
-    lta_evn
-)
+# cft_evn = classic_sta_lta(
+#     tr_evn.data,
+#     sta_evn,
+#     lta_evn
+# )
 
 # Triggers
-triggers_kkn = trigger_onset(cft_kkn, 3.0, 0.5)
-triggers_evn = trigger_onset(cft_evn, 3.0, 0.5)
+triggers_kkn = trigger_onset(cft_kkn, 3, 1)
+triggers_evn = trigger_onset(cft_evn, 3, 1.0)
 
-print("KKN sampling rate:", fs_kkn)
-print("EVN sampling rate:", fs_evn)
+if len(triggers_kkn) > 0:
+    p_pick_idx = triggers_kkn[0][0]  # First trigger index
+    p_pick_sec = p_pick_idx / fs_kkn
+    print(f"KKN P-wave onset: {p_pick_sec:.2f} s")
 
-print("\nKKN triggers:")
-for trigger in triggers_kkn:
-    print(
-        tr_kkn.stats.starttime +
-        trigger[0] / fs_kkn
-    )
-
-print("\nEVN triggers:")
-for trigger in triggers_evn:
-    print(
-        tr_evn.stats.starttime +
-        trigger[0] / fs_evn
-    )
+if len(triggers_evn) > 0:
+    p_pick_idx = triggers_evn[0][0]  # First trigger index
+    p_pick_sec = p_pick_idx / fs_evn
+    print(f"EVN P-wave onset: {p_pick_sec:.2f} s")
 
 
-import matplotlib.pyplot as plt
+fig, axes = plt.subplots(2, 1, figsize=(14, 8))
 
-plt.figure(figsize=(14, 8))
+axes[0].plot(tr_kkn.times(), tr_kkn.data)
+axes[0].set_title("KKN BHZ")
+axes[0].set_ylabel("Amplitude")
+axes[0].grid()
 
-plt.plot(
-    tr_kkn.times(),
-    cft_kkn,
-    label="KKN"
-)
+axes[1].plot(tr_evn.times(), tr_evn.data)
+axes[1].set_title("EVN BHZ")
+axes[1].set_xlabel("Seconds from 02:52:00 UTC")
+axes[1].set_ylabel("Amplitude")
+axes[1].grid()
 
-plt.plot(
-    tr_evn.times(),
-    cft_evn,
-    label="EVN"
-)
-
-plt.axhline(
-    3.0,
-    linestyle="--",
-    label="Trigger = 1.0"
-)
-
-plt.axhline(
-    0.5,
-    linestyle=":",
-    label="Release = 0.5"
-)
-
-plt.xlabel("Seconds from 02:52:00 UTC")
-plt.ylabel("STA/LTA")
-plt.title("KKN vs EVN STA/LTA")
-plt.legend()
-plt.grid()
-
+plt.tight_layout()
 plt.show()
