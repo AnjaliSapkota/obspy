249f9dfd2336e2a851dc23677d35e9a8337b5687
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-09-22 21:18:53 +0545

    distance between 3 stations

--- distance.py
+++ distance.py
@@ -1,9 +1,9 @@
 from obspy.geodetics.base import locations2degrees
 from obspy.geodetics import degrees2kilometers
 
-# Event center
-event_lat = 28.271
-event_lon = 85.515
+# eqm10
+eqm_lat = 28.299517
+eqm_lon = 83.960148
 
 # KKN
 kkn_lat = 27.800
@@ -15,33 +15,50 @@ evn_lon = 86.811653
 
 
 
-distance_kkn_deg = locations2degrees(
-    event_lat,
-    event_lon,
+distance_kkn_evn = locations2degrees(
     kkn_lat,
-    kkn_lon
-)
-
-distance_kkn_km = degrees2kilometers(distance_kkn_deg)
-
-
-
-distance_evn_deg = locations2degrees(
-    event_lat,
-    event_lon,
+    kkn_lon,
     evn_lat,
     evn_lon
 )
 
-distance_evn_km = degrees2kilometers(distance_evn_deg)
+distance_kkn_evn = degrees2kilometers(distance_kkn_evn)
+
+
+
+distance_evn_eqm = locations2degrees(
+    eqm_lat,
+    eqm_lon,
+    evn_lat,
+    evn_lon
+)
+
+distance_evn_eqm = degrees2kilometers(distance_evn_eqm)
+
+
+
+# kkn - eqm
+distance_kkn_eqm = locations2degrees(
+    kkn_lat,
+    kkn_lon,
+    eqm_lat,
+    eqm_lon
+)
+
+distance_kkn_eqm = degrees2kilometers(distance_kkn_eqm)
 
 
 
 
-print("Event → KKN")
-print("Distance:", distance_kkn_km, "km")
+print("kkn → eqm")
+print("Distance:", distance_kkn_eqm, "km")
 
 print()
 
-print("Event → EVN")
-print("Distance:", distance_evn_km, "km")
+print("eqm → EVN")
+print("Distance:", distance_evn_eqm, "km")
+
+print()
+
+print("kkn → EVN")
+print("Distance:", distance_kkn_evn, "km")
